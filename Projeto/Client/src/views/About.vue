dda13ed53a5b78517e16614581ad3479eab77f2b
Frederson <fredemanduoliveira@hotmail.com>
2021-07-03 00:18:45 -0300

    Implementa Componente Markdown to Html

--- Projeto/Client/src/views/About.vue
+++ Projeto/Client/src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <MarkHtml v-bind:archive="this.arq" />
   </div>
 </template>
+<script>
+// @ is an alias to /src
+import MarkHtml from '@/components/MarkHtml.vue';
+import mark from '!raw-loader!../../README.md';
+
+export default {
+  name: 'About',
+  data(){
+    return {
+      arq: mark
+      };
+  },
+  components: {
+    MarkHtml
+  }
+}
+</script>
