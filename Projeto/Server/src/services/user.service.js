e2f3c5db453ed82d17257a731c6a6143551f4cb1
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2021-07-14 20:30:18 -0300

    Login user

--- Projeto/Server/src/services/user.service.js
+++ Projeto/Server/src/services/user.service.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const { Types } = require('mongoose');
 const { ObjectId } = Types;
 
@@ -63,7 +64,26 @@ class UserService {
         res.status(200).json({ data: user });
     }
 
-    async loginAsync(req, res) { }
+    async loginAsync(req, res) { 
+        const data = req.body;
+        const errors = await this.getLoginErrors(data);
+        if (errors.length) return res.status(400).json({ errors });
+        
+        const defaultError = 'User or password is invalid';
+        const user = await User.findOne({ email: data.email });
+        if (!user) return res.status(400).json({ errors: [defaultError] });
+
+        const isValidPassword = bcrypt.compareSync(data.password, user.password);
+        if (!isValidPassword) return res.status(400).json({ errors: [defaultError] });
+
+        const userData = this.mapUserResponse(user);
+        const token = jwt.sign(
+            { user: userData }, 
+            process.env.SECRET, 
+            { expiresIn: parseInt(process.env.TOKEN_EXPIRATION) }
+        );
+        res.status(200).json({ data: { ...userData, token } });
+    }
 
     mapUserResponse(data) {
         return {
@@ -97,6 +117,13 @@ class UserService {
             errors.push(`Parameter type require in (${roles.Teacher}, ${roles.Student})`);
         if (data.classCode && !await Class.exists({ code: data.classCode }))
             errors.push('Class with code is not found');
+        return errors;
+    }
+
+    getLoginErrors(data) {
+        const errors = [];
+        if (!data.email) errors.push('Parameter email is required');
+        if (!data.password) errors.push('Parameter password is required');
         return errors;
     }
 }
